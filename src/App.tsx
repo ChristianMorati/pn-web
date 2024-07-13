46411e52e4850ff5cd3aea482b64e2e34236915e
christianmorati <christianmorati3@gmail.com>
2024-07-12 23:47:44 -0300

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,25 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import RootNavigator from './router/RootNavigator';
 import 'react-toastify/dist/ReactToastify.css';
-import { themeColors } from './theme/colors';
 
 const App = () => {
   return (
-    <div style={{ backgroundColor: themeColors.basePage }} className='h-[100vh]'>
-      <Provider store={store}>
-        <RootNavigator />
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
-        <ToastContainer />
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <RootNavigator />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <ToastContainer />
+    </Provider>
   );
 };
 
